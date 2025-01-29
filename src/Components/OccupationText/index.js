8f7004c94e6cdd5cef1a37058c1dc2b83f4af5d7
Austine <emidrubs@gmail.com>
2025-01-29 15:52:30 +0300

    changes on Pages to pages

--- src/Components/OccupationText/index.js
+++ src/Components/OccupationText/index.js
@@ -6,7 +6,7 @@ const OccupationText = ({ children }) => {
     <Text
       fontFamily="'Magra', sans-serif"  // Apply Magra font
       //fontSize="36px"
-      fontSize={['20px', '20px', '36px']}  // Set font size to 36px
+      fontSize={['15px', '15px', '36px']}  // Set font size to 36px
       fontWeight="400"  // Regular weight
       color="white"  // Set text color
     >
