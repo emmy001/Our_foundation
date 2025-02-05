c180a90be37da2969c8d7d09c4dd5ad2963da7a8
Austine <emidrubs@gmail.com>
2025-02-05 21:08:19 +0300

    changes on Pages to pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavigationBar from './Components/Header/Navigation'; // NavigationBar handles the navigation
 import Index from './pages';
-import EditProfile from "./pages/editProfile";
-import Auth from "./Components/Modal/Auth";
 import About from "./pages/about";
 import Impact from "./pages/impact";
 
@@ -14,8 +12,6 @@ function App(){
       <NavigationBar />
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/editProfile" element={<EditProfile />} />
-        <Route path="/Auth" element={<Auth />} />
         <Route path="/about" element={<About />} />
         <Route path="/impact" element={<Impact />} />
       </Routes>
