554e38ccc4ce882f6a697c3cba951f4896e22f13
Austine <emidrubs@gmail.com>
2025-02-20 19:21:56 +0300

    changes on Pages to pages

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,10 @@
-import { Box, Flex, Heading, Link, Button, Icon } from "@chakra-ui/react";
-import { FaBars } from "react-icons/fa";
+import { Box, Flex, Heading, Link, Button, Icon, VStack, Collapse, useDisclosure } from "@chakra-ui/react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { useState, useEffect } from "react";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const { isOpen, onToggle } = useDisclosure();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -28,6 +29,8 @@ const Header = () => {
           <Heading fontSize="2xl" fontWeight="bold" color="blue.600">
             GlobalHope Foundation
           </Heading>
+
+          {/* Desktop Navigation */}
           <Flex as="nav" gap={8} display={{ base: "none", md: "flex" }}>
             <Link href="/about" color="gray.700" _hover={{ color: "blue.600" }}>
               About
@@ -49,10 +52,41 @@ const Header = () => {
             </Link>
             <Button colorScheme="blue">Donate Now</Button>
           </Flex>
-          <Button display={{ base: "block", md: "none" }} variant="ghost">
-            <Icon as={FaBars} fontSize="2xl" />
+
+          {/* Mobile Menu Button */}
+          <Button display={{ base: "block", md: "none" }} variant="ghost" onClick={onToggle}>
+            <Icon as={isOpen ? FaTimes : FaBars} fontSize="2xl" />
           </Button>
         </Flex>
+
+        {/* Mobile Navigation Menu */}
+        <Collapse in={isOpen} animateOpacity>
+          <Box bg="white" shadow="md" p={4} display={{ md: "none" }}>
+            <VStack spacing={4} align="stretch">
+              <Link href="/about" color="gray.700" _hover={{ color: "blue.600" }} onClick={onToggle}>
+                About
+              </Link>
+              <Link href="#" color="gray.700" _hover={{ color: "blue.600" }} onClick={onToggle}>
+                Projects
+              </Link>
+              <Link href="/programs" color="gray.700" _hover={{ color: "blue.600" }} onClick={onToggle}>
+                Programs
+              </Link>
+              <Link href="/impact" color="gray.700" _hover={{ color: "blue.600" }} onClick={onToggle}>
+                Impact
+              </Link>
+              <Link href="/donate" color="gray.700" _hover={{ color: "blue.600" }} onClick={onToggle}>
+                Volunteer
+              </Link>
+              <Link href="#" color="gray.700" _hover={{ color: "blue.600" }} onClick={onToggle}>
+                Contact
+              </Link>
+              <Button colorScheme="blue" w="full" onClick={onToggle}>
+                Donate Now
+              </Button>
+            </VStack>
+          </Box>
+        </Collapse>
       </Box>
     </Box>
   );
